5b34fb8a4af4b4023524bd2782d113ccb3270240
lwy2108 <telegram @lwy01>
2021-08-14 23:19:15 +0800

    Added feature for restore config (testing) and improved main loop

--- tts_stocks_aloud.py
+++ tts_stocks_aloud.py
@@ -3,7 +3,7 @@ tts_stocks_aloud.py: A simple CLI program that speaks aloud selected stock price
 for tickers and interval.
 """
 
-from json import dump
+from json import dump, load
 from tempfile import NamedTemporaryFile
 from yahoo_fin.stock_info import get_live_price, get_market_status  # install yahoo_fin
 from gtts import gTTS  # Google Text-to-Speech, install gtts
@@ -28,7 +28,7 @@ def name_selection():
 def stock_selection():
     stock = []
     print('Stock selection: please input symbols, followed by enter after each symbol.')
-    while True:
+    while True:  # fix empty list
         pick = input('$')
         if pick == '':
             break
@@ -59,6 +59,20 @@ def backup_config():
         dump(stock, f)  # fix for name, wait
 
 
+def restore_config():
+    try:
+        with open('config.json', 'r') as f:
+            config = load(f)
+            print('Last run configuration is', config)
+            restore = input('Restore (y/n)? ')
+            if restore == 'y':
+                return config
+            else:
+                return None
+    except FileNotFoundError:
+        pass
+
+
 def request_prices():
     stock_prices = ''
     for ticker in stock:
@@ -70,7 +84,10 @@ def request_prices():
 def speak(message):
     with NamedTemporaryFile() as tf:
         tf.name = 'audio_tf.mp3'
-        audio = gTTS(text=(name + '! Stock update: ' + message), lang='en', slow=False)
+        try:
+            audio = gTTS(text=(name + '! Stock update: ' + message), lang='en', slow=False)
+        except NameError:
+            audio = gTTS(text=message, lang='en', slow=False)
         audio.save(tf.name)
         playsound(tf.name)
         tf.close()
@@ -88,22 +105,29 @@ def status_check():  # to quit on market close
         exit()
 
 
-print('This program will speak your selected stock prices at the chosen interval.')
-
-name = name_selection()
+print('This program speaks your selected stock prices at the chosen interval.')
 
 status_check()  # toggle this for testing outside market hours (valid for 1 iteration)
 
+name = name_selection()
 
-while True:
-    stock = stock_selection()
-    confirm = confirm_selection()
-    if confirm == 'c':
-        backup_config()
-        wait = wait_selection()
-        while wait:
-            stock_prices = request_prices()
-            speak(stock_prices)
-            status_check()  # toggle this to allow for more iterations for testing outside market hours
-    else:
-        continue
+stock = restore_config()
+
+while True:  # main loop
+    if not stock:
+        stock = stock_selection()
+        confirm = confirm_selection()
+        if confirm == 'c':
+            backup_config()
+        else:
+            stock = None
+            continue
+
+    wait = wait_selection()
+    while wait:
+        stock_prices = request_prices()
+        speak(stock_prices)
+        status_check()  # toggle this to allow for more iterations for testing outside market hours
+
+# implement clean exit via user input
+# parallelization to respect chosen interval
